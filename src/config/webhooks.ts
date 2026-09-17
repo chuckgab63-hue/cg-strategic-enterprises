f18e288933d14ece8bbe14f20244df5abc15ede6
Charles <chuckgab63@gmail.com>
2026-09-17 03:59:19 -0400

    Fix missing webhooks.ts update

--- src/config/webhooks.ts
+++ src/config/webhooks.ts
@@ -6,3 +6,6 @@ export const CONTACT_WEBHOOK_URL = 'https://hook.us2.make.com/8rfha8cgdlsmtnrcnt
 export const SITE_PROGRESS_WEBHOOK_URL = 'https://hook.us2.make.com/kkth7gf6ldafbvwo2y4squd3chvxnj6w';
 export const MAINTENANCE_WEBHOOK_URL = 'https://hook.us2.make.com/c1afkyv1s24u1zys40nv8zu6yzqbbfqd';
 export const SHOWING_WEBHOOK_URL = 'https://hook.us2.make.com/dyk9yh1vnzho0icm9djhdxvx5qyyt6lk';
+export const ASSET_DROP_OFF_WEBHOOK_URL = 'https://hook.us2.make.com/uy2blad99b3bqxnxrt1k6mq3qysjvkis';
+export const ASSET_RETURN_WEBHOOK_URL = 'https://hook.us2.make.com/87znwz8ftrqpsmfwc49bf533g2kufs0l';
+export const ASSET_GET_LOCATIONS_WEBHOOK_URL = 'https://hook.us2.make.com/ohd5tlttrqstc7y4cg9377fwkncy5scv';
